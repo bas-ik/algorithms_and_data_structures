565dbd30ca39b6839bace3b597d88e2bdc1fb754
bas_ik <e.v.novozhilov@yandex.ru>
2021-09-18 13:40:05 +0300

    feat: add get, set, pop, methods; add additional checks in constructor

--- list/List.py
+++ list/List.py
@@ -3,41 +3,81 @@ from list.ListElement import _ListElement
 
 class List:
     def __initialize_element(self, data):
-        self.head = _ListElement(data)
+        """
+        method to initialize List by variable
+        :param data: float (int) value
+        :return: the "head" of List
+        """
+        self._head = _ListElement(data)
 
     def __initialize_array(self, data):
-        self.head = _ListElement(data[0])
-        tmp = self.head
+        """
+        private method to initialize List by array
+        :param data: list of int (float) variables
+        :return: the "head" of list
+        """
+        self._head = _ListElement(data[0])
+        tmp = self._head
         for i in data[1:]:
             tmp.next_node = _ListElement(i)
             tmp = tmp.next_node
 
     def __init__(self, data):
+        """
+        constructor: initialize List
+
+        :param data: array (list) or float (int) variable
+        """
         if type(data) is list:
-            print("it's list")
+            for i in data:
+                if (type(i) is not int) and (type(i) is not float):
+                    raise TypeError("expected list of float (int) values")
             self.__initialize_array(data)
         elif (type(data) is float) or (type(data) is int):
             self.__initialize_element(data)
-            print("it's float/int variable")
         else:
             raise TypeError('expected array (list) or float (int) variable')
 
     def __len__(self):
-        if self.head is None:
+        """
+        find the len of List
+        :return: int value, that containing the len of List
+        """
+        if self._head is None:
             return 0
-        reference, list_len = self.head, 0
+        reference, list_len = self._head, 0
         while reference.next_node is not None:
             reference = reference.next_node
             list_len += 1
         return list_len + 1
 
     def __str__(self):
-        reference, string = self.head, ""
+        """
+        converts a List to a string. Method is used for printing List
+
+        :return: string containing data of all List element
+        """
+        reference, string = self._head, ""
         while reference is not None:
             string += str(reference.data) + '->'
             reference = reference.next_node
         string += "None"
         return string
+
+    def get(self, index):
+        """
+        :return value by index
+
+        :param index: index of return's element
+        :return: data
+        """
+        number, reference = 0, self._head
+        while (reference is not None) and (number != index):
+            reference = reference.next_node
+            number += 1
+        if number != index:
+            raise ValueError("index doesn\'t exist!")
+        return reference.data
 
     def append(self, data):
         """
@@ -46,20 +86,20 @@ class List:
         :param data: data to insert into the end of the list
         :return: None
         """
-        reference = self.head
+        reference = self._head
         while reference.next_node is not None:
             reference = reference.next_node
         reference.next_node = _ListElement(data)
 
     def insert(self, index, data):
         """
-        insert a element to the list
+        insert a element to the List
 
         :param index: the index where the element needs to be inserted
         :param data: data to insert into the list
-        :return: the head of the list
+        :return: the head of the List
         """
-        number, reference = 0, self.head
+        number, reference = 0, self._head
         while (reference is not None) and (number != index):
             reference = reference.next_node
             number += 1
@@ -71,3 +111,22 @@ class List:
             reference.data, tmp = tmp, reference.data
         last_elem = _ListElement(tmp)
         reference.next_node = last_elem
+
+    def pop(self, index):
+        """
+        del element from List by index
+
+        :param index: index of del element
+        """
+        if index == 0:
+            self._head = self._head.next_node
+        else:
+            number, reference = 0, self._head
+            while (reference is not None) and (number != index - 1):
+                reference = reference.next_node
+                number += 1
+            if number != index - 1:
+                raise ValueError('index doesn\'t exist')
+            del_elem = reference.next_node
+            reference.next_node = del_elem.next_node
+            del del_elem
--- main.py
+++ main.py
@@ -4,13 +4,19 @@ TODO func of deleting. fix deleting first element
 TODO solve optional task
 """
 
+"""
+поводы доебаться до Айдана
+1) insert or set?
+2) del 0 element
+"""
 
 from list.List import List
 
 if __name__ == "__main__":
     a = List([1, 2, 3])
-    # a.print_list()
-    # print(type(a.list))
-    print(type(a))
     a.insert(2, 100)
+    a.insert(3, 125)
+    print(a)
+    a.pop(0)
+    a.pop(3)
     print(a)
