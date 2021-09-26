ce867f85b64bb0d6001f9d08f399a15007618c09
bas_ik <e.v.novozhilov@yandex.ru>
2021-09-26 23:53:24 +0300

    fix(Stack): new way of saving data: data store as reverse list, and head of list is last element under-hood

--- stack/Stack.py
+++ stack/Stack.py
@@ -4,14 +4,25 @@ from list.List import List
 class Stack:
 
     def __init__(self, data):
-        self.stack = List(data)
+        self._stack = List(data[::-1])
+        self.last_elem = self._stack.get(0)
 
-    def __str__(self):
-        return str(self.stack)[:-6]     # slice is required not to print "->None"
+    def __str__(self):    # 3->2->5
+        string = str(self._stack)[:-6]     # slice is required not to print "->None"
+        values = string.split('->')
+        string = ""
+        for i in [str(i) + "->" for i in values[::-1]]:
+            string += i
+        return string[:-2]
 
     def pop(self):
-        self.stack.pop()
+        self._stack.remove(0)
+        self.last_elem = self._stack.get(1)
 
     def pushback(self, data):
-        self.stack.append(data)
+        self._stack.insert(0, data)
+        self.last_elem = self._stack.get(0)
+
+    def get(self, index):
+        return self._stack.get(index)
 
