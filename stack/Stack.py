c729e4ee5780bcfb657f6f244fc023259c335f79
bas_ik <e.v.novozhilov@yandex.ru>
2021-09-27 03:37:09 +0300

    feat (Stack): add some checking to pushback and pop methods

--- stack/Stack.py
+++ stack/Stack.py
@@ -3,23 +3,31 @@ from list.List import List
 
 class Stack:
 
-    def __init__(self, data):
-        self._stack = List(data[::-1])
-        self.last_elem = self._stack.get(0)
-
-    def __str__(self):  # 3->2->5
-        string = str(self._stack)[:-6]  # slice is required not to print "->None"
+    def __str__(self):
+        string = str(self._stack)
         values = string.split('->')
         string = ""
         for i in [str(i) + "->" for i in values[::-1]]:
             string += i
         return string[:-2]
 
+    def __init__(self, data):
+        self._stack = List(data[::-1])
+        self.last_elem = self._stack.get(0)
+
     def pop(self):
-        self._stack.remove(0)
-        self.last_elem = self._stack.get(1)
+        if self.last_elem is not None:
+            element = self.last_elem
+            self._stack.remove(0)
+            self.last_elem = self._stack.get(0)
+            return element
+        else:
+            return None
 
     def pushback(self, data):
+        # if self._stack.get(0) is None:
+        #     self._stack.set(0, data)
+        # else:
         self._stack.insert(0, data)
         self.last_elem = self._stack.get(0)
 
