596a4b347ffc22e8b7d2858269b136b8b24a9b9a
bas_ik <e.v.novozhilov@yandex.ru>
2021-09-30 20:14:19 +0300

    feat (main): add some demonstration of functional as example

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from list.List import List
 from my_array.Array import MyArray
-
+from stack.Stack import Stack
+from algorithms.infix_to_postfix.Infix_to_postfix import InfixToPostfix as ITP
 
 if __name__ == "__main__":
-    a = List([1, 2, 3])
-    print(a.get(2))
+    a = List([10, 20, 30, 40])
+    print(a)
+    a.append(10)
+    print(a)
+    a.pop()
+    print(a)
+
+    b = MyArray([1, 2, 3])
+    print(b)
+
+    c = Stack([1, 2, 3])
+    print(c)    # последний элемент стека -- самый правый
+
+    algorithm = ITP("10 + sin(20) - cos(30 * 4) ^ 5")
+    algorithm.start()   # запуск перевода
