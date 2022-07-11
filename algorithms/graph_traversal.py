915ee6f2c76520b62f290bb5a6a18f18f859c5ea
bas_ik <e.v.novozhilov@yandex.ru>
2022-07-11 15:07:11 +0300

    feat: add parser and opportunity to collect python files

--- algorithms/graph_traversal.py
+++
@@ -1,6 +0,0 @@
-from disjointset.DisjointSetElement import DisjointSetElement
-from my_array.Array import MyArray
-from edge.Edge import Edge
-from algorithms.sort.buble_sort_for_kruskal import Sort
-from deque.queue import Queue
-
--- main.py
+++
@@ -1,8 +0,0 @@
-from stack.Stack import Stack
-from algorithms.tim_sort.run import Run
-from algorithms.tim_sort.sorting import Sorting
-
-if __name__ == "__main__":
-    a = [11, 10, 9, 6, 3, 2, 0]
-    Sorting.sort(a)
-    print(a)
